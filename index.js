ed7182d4675838e2bc9e1792aa416d311cb2599a
Elan Shanker <elan.shanker@gmail.com>
2014-10-27 15:33:45 -0400

    Add custom callback optional arg

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 var Stopwatch = require('timer-stopwatch');
 
-function Hangman(timeLimit) {
+function Hangman(timeLimit, callback) {
+  if (typeof timeLimit === 'function') {
+    callback = timeLimit;
+    timeLimit = void 0;
+  }
   if (!timeLimit) { timeLimit = 60000; }
   var timer = new Stopwatch(timeLimit, {
     refreshRate: Math.floor(timeLimit / 10) || 10
   });
-  timer.on('done', function() {
+  timer.on('done', callback || function() {
     process.exit(1);
   });
   timer.start();
