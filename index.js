d39ed64e4b45e0a7d7fee8eed3e803a6af359bf3
Elan Shanker <elan.shanker@gmail.com>
2014-10-29 16:38:04 -0400

    callback vs monitor & stdout vs heartbeat

--- index.js
+++ index.js
@@ -5,29 +5,48 @@ var path = require('path');
 var Stopwatch = require('timer-stopwatch');
 var stdout = new (require('fixture-stdout'))();
 
+var monitor;
+function getMonitor() {
+  return monitor = monitor ||
+    spawn(path.join(__dirname, 'monitor.js'), [], { stdio: ['pipe', 1, 2] });
+}
+
 function Hangman(timeLimit, callback) {
   if (typeof timeLimit === 'function') {
     callback = timeLimit;
     timeLimit = void 0;
   }
   if (!timeLimit) { timeLimit = 60000; }
-  var monitor = spawn(path.join(__dirname, 'monitor.js'), [], { stdio: ['pipe', 1, 2] });
-  stdout.capture(function() {
-    monitor.stdin.write('stdout');
-  });
-  var timer = new Stopwatch(timeLimit, {
-    refreshRate: Math.floor(timeLimit / 10) || 10
-  });
-  timer.on('done', callback || function() {
-    process.exit(1);
-  });
-  timer.start();
+  var timer;
+  if (callback) {
+    timer = new Stopwatch(timeLimit, {
+      refreshRate: Math.floor(timeLimit / 10) || 10
+    });
+    timer.on('done', callback || function() {
+      process.exit(1);
+    });
+    timer.start();
+  } else {
+    getMonitor();
+    var stdin = monitor.stdin;
+    timer = {
+      start: Function.prototype,
+      stop: stdin.write.bind(stdin, {stop: true}),
+      reset: stdin.write.bind(stdin, {reset: true})
+    };
+  }
   function heartbeat() {
     timer.reset();
     timer.start();
   }
-  heartbeat.cancel = timer.stop;
-  return heartbeat;
+  stdout.capture(heartbeat);
+  var stay = function () {
+    stdout.release();
+    heartbeat();
+    stay = heartbeat;
+  };
+  stay.cancel = heartbeat.cancel = timer.stop;
+  return stay;
 }
 
 module.exports = Hangman;
