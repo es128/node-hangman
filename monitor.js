8616243231aa2dc4567d2f7f9d0b90d51c0b04f1
Elan Shanker <elan.shanker@gmail.com>
2014-11-01 12:06:53 -0400

    Set up monitor

--- monitor.js
+++ monitor.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
+'use strict';
 
-process.stdin.on('data', function(){console.log('received')})
+var hangman = require('./');
+var monitor;
+
+process.stdin.on('data', function(msg){
+	if (!hangman) {
+		var msgObj;
+		try {
+			msgObj = JSON.parse(msg);
+		} catch (_e) { return; }
+		monitor = hangman(
+			msgObj.timeLimit,
+			process.kill.bind(process, msgObj.pid)
+		);
+	} else if (msg === 'reset') {
+		monitor();
+	} else if (msg === 'stop') {
+		monitor.cancel();
+	}
+});
