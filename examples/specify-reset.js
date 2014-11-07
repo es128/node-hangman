fa84cdf78023cc874494eaa66cc9bed4c8fada41
Elan Shanker <elan.shanker@gmail.com>
2014-11-07 15:32:09 -0500

    Add an non-stdout reset example

---
+++ examples/specify-reset.js
@@ -0,0 +1,13 @@
+var hangmanReset = require('../hangman')(3000);
+
+console.log('Hangman initiated with a 3s timeout and custom callback');
+console.log('Using app-specific timer reset which cancels stdout monitoring');
+hangmanReset();
+var i = 0;
+setInterval(function(){
+	if (i === 10) {
+		console.log('Resetting one last time, will be killed in 3s');
+		hangmanReset();
+	}
+	process.stdout.write(++i + '\r');
+}, 100);
